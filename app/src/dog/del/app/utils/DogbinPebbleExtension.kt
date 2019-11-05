256f88db6c0d32e3dec78333aa0c594256fb2241
Till Kottmann <me@deletescape.ch>
2019-11-05 21:17:21 +0100

    Display stats only if enabled and available
    Signed-off-by: Till Kottmann <me@deletescape.ch>

--- app/src/dog/del/app/utils/DogbinPebbleExtension.kt
+++ app/src/dog/del/app/utils/DogbinPebbleExtension.kt
@@ -1,6 +1,7 @@
 package dog.del.app.utils
 
 import com.mitchellbosecke.pebble.extension.AbstractExtension
+import dog.del.app.config.AppConfig
 import dog.del.app.stats.StatisticsReporter
 import dog.del.commons.Date
 import dog.del.commons.year
@@ -13,10 +14,12 @@ class DogbinPebbleExtension : AbstractExtension(), KoinComponent {
     private val store by inject<TransientEntityStore>()
     private val config by inject<Config>()
     private val reporter by inject<StatisticsReporter>()
+    private val appConfig by inject<AppConfig>()
 
     override fun getGlobalVariables(): MutableMap<String, Any> = mutableMapOf(
         "config" to config.freezeCached(store),
         "year" to Date.getInstance().year,
-        "stats_embed" to reporter.embedCode
+        "stats_embed" to reporter.embedCode,
+        "appConfig" to appConfig
     )
 }
